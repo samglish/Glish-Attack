24cba98a342ab33b1563a83a09c5f32759362102
BEIDI DINA SAMUEL <77502731+samglish@users.noreply.github.com>
2025-03-23 12:44:14 -0400

    update

--- GlishWireless.sh
+++ GlishWireless.sh
@@ -1,11 +1,26 @@
 !/bin/bash
 
+echo "*******************************************************************************"
+echo "**                     APPLICATION DE BEIDI DINA SAMUEL                      **"
+echo "**                        Ethical Hacking - Pentest                          **"
+echo "*******************************************************************************"
+echo "**                  https://github.com/samglish/Glish-Attack                 **"
+echo "*******************************************************************************"
+echo ""
+echo "1. Wireless Show"
+echo "2. Wireless Attack"
+echo "3. Wireless Logout"
+echo "4. Wireless Connect"
+echo "5. My interface"
+echo ""
+echo "*******************************************************************************"
+
 read -p "Selectionnez une option (1, 2 ou 3) : " option
 # Remplacez par la valeur que vous voulez tester
 
 case $option in
     1)
-        echo "Option 1 sélectionnée."
+        echo "Wireless"
         ;;
     2)
         echo "Option 2 sélectionnée."
